c49caf806ab26579a57fea24a55a83d8e7d003d0
Ranielly Ferreira <eu@raniellyferreira.com.br>
2020-11-22 13:58:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Serveless proxy enable cors
+
+Serverless proxy for enable cors written in Golang
+
+Add this headers to response: Access-Control-Allow-Origin: *, Access-Control-Allow-Methods: *, Access-Control-Allow-Headers: *
+
+https://enable-cors-proxy.vercel.app/api?u=SUA-URL
+
+```sh
+$ curl https://enable-cors-proxy.vercel.app/api?u=https://economia.awesomeapi.com.br/all/usd
+```
---
+++ api/index.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	targetUrl := r.URL.Query().Get("u")
+	if targetUrl == "" {
+		http.Error(w, "url ?u= is required", http.StatusBadRequest)
+		return
+	}
+
+	url, err := url.Parse(r.URL.Query().Get("u"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req := &http.Request{
+		URL:    url,
+		Method: r.Method,
+		Body:   r.Body,
+		Header: r.Header,
+	}
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+
+	copyHeaders(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	addCorsHeaders(w)
+
+	io.Copy(w, resp.Body)
+}
+
+func addCorsHeaders(w http.ResponseWriter) {
+	w.Header().Del("Access-Control-Allow-Origin")
+	w.Header().Del("Access-Control-Allow-Methods")
+	w.Header().Del("Access-Control-Allow-Headers")
+
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "*")
+}
+
+func copyHeaders(dst, src http.Header) {
+	for k, vv := range src {
+		dst.Add(k, strings.Join(vv, ","))
+	}
+}
